f49be8fbfd352cdfe1a70c2ab1d5b758b540e0d9
ducnguyenND <ducnguyenbvtvc@gmail.com>
2022-08-08 16:53:06 +0700

    up

--- web bán đt/js/logout.js
+++ web bán đt/js/logout.js
@@ -1,27 +1,3 @@
-
-
-    // let token = localStorage.getItem("token");
-    // $.ajax({
-    //     type: "GET",
-    //     headers: {
-    //         'Accept': 'application/json'
-    //     },
-    //     beforeSend: function (xhr) {
-    //         xhr.setRequestHeader ("Authorization", "Bearer " + token);
-    //     },
-    //     url: "http://localhost:8180/logout",
-    //     success: function (data) {
-    //         console.log("da dang xuat")
-    //         console.log("da dang xuat")
-    //         console.log("da dang xuat")
-    //         // localStorage.setItem("token", "");
-    //     },
-    //     error: function (err) {
-    //         console.log("loiiiiiii")
-    //         location.href = "login.html"
-    //     }
-    // })
-
 function logout(){
     localStorage.clear()
     location.href = "shop.html"
